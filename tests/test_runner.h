24aa76afb3d9790da765468f09629acfb51d4057
Artur Evdokimov <75699486+evd0903@users.noreply.github.com>
2024-03-10 03:04:13 +0300

    Update test_runner.h

--- tests/test_runner.h
+++ tests/test_runner.h
@@ -4,60 +4,82 @@
 #include <sstream>
 #include <string>
 
-#include "../src/AdvAuction.h"
+#include "AdvAuction.h"
 
-std::ostream& operator<<(std::ostream& os, const std::vector<int> vec) {
-  os << "{";
-  for (int i = 0; i < vec.size(); ++i) {
-    os << vec[i] << " ";
-  }
-  os << "}";
+template <typename U>
+std::ostream& operator<<(std::ostream& os, const std::vector<U> vec) {
+    os << "{";
+    for (int i = 0; i < vec.size(); ++i) {
+        os << vec[i] << " ";
+    }
+    os << "}" << std::endl;
 
-  return os;
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const Banner banner) {
+    os << "{";
+    os << banner.price << " " << banner.campaign_id;
+    os << "{";
+    for (int i = 0; i != banner.countries.size(); ++i) {
+        os << banner.countries[i] << " ";
+    }
+    os << "} ";
+    os << banner.internal_id;
+    os << "}" << std::endl;
+
+    return os;
+}
+
+bool operator==(const Banner& left, const Banner& right) {
+    return left.price == right.price && left.campaign_id == right.campaign_id
+        && left.countries == right.countries && left.internal_id == right.internal_id;
 }
 
 template <class T, class U>
 void AssertEqual(const T& t, const U& u, const std::string& hint = {}) {
-  if (!(t == u)) {
-    std::ostringstream os;
-    os << "Assertion failed: " << t << " != " << u;
-    if (!hint.empty()) {
-      os << " hint: " << hint;
+    if (!(t == u)) {
+        std::ostringstream os;
+        os << "Assertion failed: " << t << " != " << u;
+        if (!hint.empty()) {
+            os << " hint: " << hint;
+        }
+        throw std::runtime_error(os.str());
     }
-    throw std::runtime_error(os.str());
-  }
 }
 
 inline void Assert(bool b, const std::string& hint) {
-  AssertEqual(b, true, hint);
+    AssertEqual(b, true, hint);
 }
 
 class TestRunner {
 public:
-  template <class TestFunc>
-  void RunTest(TestFunc func, const std::string& test_name) {
-    try {
-      func();
-      std::cerr << test_name << " OK" << std::endl;
-    } catch (std::exception& e) {
-      ++fail_count;
-      std::cerr << test_name << " fail: " << e.what() << std::endl;
-    } catch (...) {
-      ++fail_count;
-      std::cerr << "Unknown exception caught" << std::endl;
+    template <class TestFunc>
+    void RunTest(TestFunc func, const std::string& test_name) {
+        try {
+            func();
+            std::cerr << test_name << " OK" << std::endl;
+        }
+        catch (std::exception& e) {
+            ++fail_count;
+            std::cerr << test_name << " fail: " << e.what() << std::endl;
+        }
+        catch (...) {
+            ++fail_count;
+            std::cerr << "Unknown exception caught" << std::endl;
+        }
     }
-  }
 
-  ~TestRunner() {
-    std::cerr.flush();
-    if (fail_count > 0) {
-      std::cerr << fail_count << " unit tests failed. Terminate" << std::endl;
-      exit(1);
+    ~TestRunner() {
+        std::cerr.flush();
+        if (fail_count > 0) {
+            std::cerr << fail_count << " unit tests failed. Terminate" << std::endl;
+            exit(1);
+        }
     }
-  }
 
 private:
-  int fail_count = 0;
+    int fail_count = 0;
 };
 
 #ifndef FILE_NAME
